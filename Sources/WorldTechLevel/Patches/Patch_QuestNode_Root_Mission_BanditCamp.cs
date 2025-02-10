d7b1f2fb4d64c82d8b010b20158fb23d55a3ea4a
m00nl1ght <github@m00nl1ght.dev>
2025-02-10 18:23:30 +0100

    Fix bandit camp faction patch

--- Sources/WorldTechLevel/Patches/Patch_QuestNode_Root_Mission_BanditCamp.cs
+++ Sources/WorldTechLevel/Patches/Patch_QuestNode_Root_Mission_BanditCamp.cs
@@ -18,10 +18,16 @@ internal static class Patch_QuestNode_Root_Mission_BanditCamp
     [HarmonyPatch(nameof(QuestNode_Root_Mission_BanditCamp.TryGetSiteFaction))]
     internal static void TryGetSiteFaction_Postfix(ref Faction faction, ref bool __result)
     {
-        if (!__result && Find.FactionManager.AllFactions.Where(f => !f.temporary && f.HostileTo(Faction.OfPlayer)).TryRandomElement(out faction))
+        if (!__result && Find.FactionManager.AllFactions.Where(CanUseFaction).TryRandomElement(out faction))
         {
             WorldTechLevel.Logger.Log($"Selecting faction {faction.name} for QuestNode_Root_Mission_BanditCamp");
             __result = true;
         }
+    }
+
+    private static bool CanUseFaction(Faction faction)
+    {
+        if (faction.temporary || !faction.HostileTo(Faction.OfPlayer)) return false;
+        return faction.def.pawnGroupMakers?.Any(p => p.kindDef == PawnGroupKindDefOf.Settlement) ?? false;
     }
 }
