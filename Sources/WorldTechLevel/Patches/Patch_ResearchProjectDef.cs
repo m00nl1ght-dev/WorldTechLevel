eedf43650dcf02872de8a5e2d2694bf837a14d2e
m00nl1ght <github@m00nl1ght.dev>
2025-02-27 18:35:42 +0100

    Add patch for research CanStartNow

---
+++ Sources/WorldTechLevel/Patches/Patch_ResearchProjectDef.cs
@@ -0,0 +1,20 @@
+using HarmonyLib;
+using LunarFramework.Patching;
+using Verse;
+
+namespace WorldTechLevel.Patches;
+
+[PatchGroup("Filters")]
+[HarmonyPatch(typeof(ResearchProjectDef))]
+internal static class Patch_ResearchProjectDef
+{
+    [HarmonyPrepare]
+    private static bool IsFilterEnabled() => WorldTechLevel.Settings.Filter_Research;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(ResearchProjectDef.CanStartNow), MethodType.Getter)]
+    internal static void CanStartNow_Postfix(ResearchProjectDef __instance, ref bool __result)
+    {
+        __result = __result && __instance.EffectiveTechLevel() <= TechLevelUtility.PlayerResearchFilterLevel();
+    }
+}
